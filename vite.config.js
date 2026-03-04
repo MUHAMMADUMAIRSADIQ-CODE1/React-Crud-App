352d423667e17b5b6b3fb6f2098efc70a5f31c3c
MUHAMMADUMAIRSADIQ-CODE1 <ghulamrabia0048@gmail.com>
2026-03-04 07:55:05 +0500

    updation

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Crud-App/",   // 👈 yahan apne repo ka naam likhna hai
+  base: "/React-Crud-App/",   
 })
